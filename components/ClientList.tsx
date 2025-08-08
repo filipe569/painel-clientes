5abeabc11cc56879f0a903f5edc2789d31b7e96e
filipe569 <phillip_suk@hotmail.com>
2025-08-07 21:41:06 -0300

    Updated ClientList.tsx

--- components/ClientList.tsx
+++ components/ClientList.tsx
@@ -1,21 +1,22 @@
-@@ .. @@
- interface ClientListProps {
-   clients: Client[];
-   onEdit: (client: Client) => void;
-   onDelete: (id: string) => void;
-+  mercadoPagoEnabled?: boolean;
- }
- 
--export default function ClientList({ clients, onEdit, onDelete }: ClientListProps) {
-+export default function ClientList({ clients, onEdit, onDelete, mercadoPagoEnabled }: ClientListProps) {
-   return (
-@@ .. @@
-         {clients.map((client) => (
-           <ClientRow
-             key={client.id}
-             client={client}
-             onEdit={onEdit}
-             onDelete={onDelete}
-+            mercadoPagoEnabled={mercadoPagoEnabled}
-           />
-         ))}
+interface ClientListProps {
+  clients: Client[];
+  onEdit: (client: Client) => void;
+  onDelete: (id: string) => void;
+  mercadoPagoEnabled?: boolean;
+}
+
+export default function ClientList({ clients, onEdit, onDelete, mercadoPagoEnabled }: ClientListProps) {
+  return (
+        {clients.map((client) => (
+          <ClientRow
+            key={client.id}
+            client={client}
+            onEdit={onEdit}
+            onDelete={onDelete}
+            mercadoPagoEnabled={mercadoPagoEnabled}
+          />
+        ))}
+      </tbody>
+    </table>
+  );
+}
